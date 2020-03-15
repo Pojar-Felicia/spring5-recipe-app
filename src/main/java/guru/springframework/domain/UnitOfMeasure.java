23402d8da81699b378e66852b06461bf66604be3
Pojar-Felicia <pojarfelicia@gmail.com>
2020-03-15 22:23:02 +0200

    Creating Spring Data Repository

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -8,8 +8,6 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-//    @OneToOne
-//    private Ingredient ingredient;
 
     public Long getId() {
         return id;
@@ -26,12 +24,4 @@ public class UnitOfMeasure {
     public void setDescription(String description) {
         this.description = description;
     }
-
-//    public Ingredient getIngredient() {
-//        return ingredient;
-//    }
-//
-//    public void setIngredient(Ingredient ingredient) {
-//        this.ingredient = ingredient;
-//    }
 }
