7d4643352ce26b4738a2bc252a64d1a53bc40538
Pojar-Felicia <pojarfelicia@gmail.com>
2020-03-15 23:10:23 +0200

    Spring Data JPA Query Methods

--- src/main/java/guru/springframework/Controller/IndexController.java
+++ src/main/java/guru/springframework/Controller/IndexController.java
@@ -12,20 +12,20 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-//    private CategoryRepository categoryRepository;
-//    private UnitOfMeasureRepository unitOfMeasureRepository;
-//
-//    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-//        this.categoryRepository = categoryRepository;
-//        this.unitOfMeasureRepository = unitOfMeasureRepository;
-//    }
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
-//        Optional<Category > categoryOptional = categoryRepository.findByDescription("American");
-//        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-//        System.out.println("Cat Id is: "+ categoryOptional.get().getId());
-//        System.out.println("UOM id is: "+ unitOfMeasureOptional.get().getId());
+        Optional<Category > categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        System.out.println("Cat Id is: "+ categoryOptional.get().getId());
+        System.out.println("UOM id is: "+ unitOfMeasureOptional.get().getId());
         return "index";
     }
 
