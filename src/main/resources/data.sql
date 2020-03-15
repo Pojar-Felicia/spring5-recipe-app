9ceab11567ccf39a7b3da26d7ac313721b48c47a
Pojar-Felicia <pojarfelicia@gmail.com>
2020-03-15 23:09:22 +0200

    Database Initialization with Spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUE ('American');
+INSERT INTO category (description) VALUE ('Italian');
+INSERT INTO category (description) VALUE ('Mexican');
+INSERT INTO category (description) VALUE ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUE ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUE ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUE ('Cup');
+INSERT INTO unit_of_measure (description) VALUE ('Pinch');
+INSERT INTO unit_of_measure (description) VALUE ('Ounce');
